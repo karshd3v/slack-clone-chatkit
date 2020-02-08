bc5d8c7210767826cda88e356ce895691dcbad5e
Utkarsh Mishra <iamutkarshmishra@gmail.com>
2020-02-08 17:18:49 +0530

    added login and chat screen

--- server.js
+++ server.js
@@ -1,12 +1,40 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const Chatkit = require('@pusher/chatkit-server')
 
 const app = express()
+
+const chatkit = new Chatkit.default({
+  instanceLocator: 'v1:us1:2ca6cc63-973e-49ba-b260-9c3ece933171',
+  key: '0e7802a1-169b-4274-9f22-cc034171fbf5:L6FHvKnHBfGnhzBFRdNflxn4vk1S5rc8iH4HznniYfM=',
+});
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
+
+app.post('/users', (req, res) => {
+  const { username } = req.body
+  chatkit
+    .createUser({
+      id: username,
+      name: username
+    })
+    .then(() => res.sendStatus(201))
+    .catch(error => {
+      if (error.error === 'services/chatkit/user_already_exists') {
+        res.sendStatus(200)
+      } else {
+        res.status(error.status).json(error)
+      }
+    })
+})
+
+app.post('/authenticate', (req, res) => {
+  const authData = chatkit.authenticate({ userId: req.query.user_id })
+  res.status(authData.status).send(authData.body)
+})
 
 const PORT = 3001
 app.listen(PORT, err => {
